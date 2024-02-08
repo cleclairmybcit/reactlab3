14d537cfd6da88a35f90976eef26267a355083cf
cleclairmybcit <113483736+cleclairmybcit@users.noreply.github.com>
2024-02-08 13:17:32 -0800

    more improves

--- assignment-tracker/src/components/Assignment/index.tsx
+++ assignment-tracker/src/components/Assignment/index.tsx
@@ -3,19 +3,18 @@ import { TbTrash } from "react-icons/tb";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 
 export function Assignment({i, tasks, setTasks}: Tasks) {
-  const done = tasks[i ?? 0].done;
-  const title = tasks[i ?? 0].title;
+  const task = tasks[i ?? 0];
   const del = () => {setTasks(tasks.filter((_, j) => j !== i))};
   const toggle = () => {
-    setTasks(tasks.map((task, j) => (j === i ? { ...task, done: !task.done } : task)));
+    setTasks(tasks.map((t, j) => (j === i ? { ...t, done: !t.done } : t)));
   };
 
   return (
     <div className={styles.assignment}>
       <button className={styles.checkContainer} onClick={toggle}>
-        {done ? <div><BsFillCheckCircleFill/></div> : <div />}
+        {task.done ? <div><BsFillCheckCircleFill/></div> : <div />}
       </button>
-      <p className={done ? styles.textCompleted : undefined}>{title}</p>
+      <p className={task.done ? styles.textCompleted : undefined}>{task.title}</p>
       <button className={styles.deleteButton} onClick={del}>
         <TbTrash size={20} />
       </button>
